4ae8810b009df73ba7f865a072c5930f20d27b1c
cjswo-duddn <cjswoudddn@naver.com>
2021-02-22 14:40:15 +0900

    2021 02 22

---
+++ src/main/java/com/appeal/api/common/exception/NoSupportDtoException.java
@@ -0,0 +1,6 @@
+package com.appeal.api.common.exception;
+
+public class NoSupportDtoException extends RuntimeException {
+    public NoSupportDtoException(String s) {
+    }
+}
---
+++ src/main/java/com/appeal/api/config/RedisConfiguraion.java
@@ -0,0 +1,23 @@
+package com.appeal.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfiguraion {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(){
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new LettuceConnectionFactory(config);
+    }
+}
