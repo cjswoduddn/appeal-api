703e646a2343377ad0124e3d37d6e00ab4e80101
cjswo-duddn <cjswoudddn@naver.com>
2021-03-15 22:25:33 +0900

    cors test 4

--- appeal-api/src/main/java/com/appeal/api/security/handler/CustomAuthenticationSuccessHandler.java
+++ appeal-api/src/main/java/com/appeal/api/security/handler/CustomAuthenticationSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.appeal.api.security.handler;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
@@ -7,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collection;
 
 public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
@@ -14,6 +16,21 @@ public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_OK);
-//        response.getOutputStream().print(((Member)authentication.getPrincipal()).getId());
+        addSameSiteCookieAttribute(response);
+    }
+    private void addSameSiteCookieAttribute(HttpServletResponse response) {
+        Collection<String> headers = response.getHeaders(HttpHeaders.SET_COOKIE);
+        boolean firstHeader = true;
+        // there can be multiple Set-Cookie attributes
+        for (String header : headers) {
+            if (firstHeader) {
+                response.setHeader(HttpHeaders.SET_COOKIE,
+                        String.format("%s; %s", header, "SameSite=Strict"));
+                firstHeader = false;
+                continue;
+            }
+            response.addHeader(HttpHeaders.SET_COOKIE,
+                    String.format("%s; %s", header, "SameSite=Strict"));
+        }
     }
 }
