cd405b674ee9cd2bab841e80f171073fa1a61def
cjswo-duddn <cjswoudddn@naver.com>
2021-03-15 20:55:10 +0900

    cors test3

--- appeal-api/src/main/java/com/appeal/api/security/config/Config.java
+++ appeal-api/src/main/java/com/appeal/api/security/config/Config.java
@@ -47,7 +47,7 @@ public class Config {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.addAllowedOrigin("*");
+        configuration.addAllowedOriginPattern("*");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
