b557e20165a75f9fb478505500ce3bb5abdae253
cjswo-duddn <cjswoudddn@naver.com>
2021-05-13 13:41:55 +0900

    update deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,36 +3,21 @@
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=appeal-api-webservice
 
-echo "> Build 파일 복사"
+echo "build!"
+cp $REPOSITORY/zip/*.jar $REPOSITORY
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+echo "what is current java process ID?"
+CURRENT_PID=$(pgrep java)
+echo "$CURRENT_PID"
 
-echo "> 현재 구동중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -fl appeal-api-webservice | grep jar | awk '{print $1}')
-
-echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-    echo "> kill -9 $CURRENT_PID"
-    kill -9 $CURRENT_PID
-    sleep 5
+if [ ! -z "$CURRENT_PID" ]; then
+  kill -9 $CURRENT_PID
+  sleep  5
 fi
 
-echo "> 새 어플리케이션 배포"
-
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
-echo "> JAR Name: $JAR_NAME"
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
 chmod +x $JAR_NAME
 
-echo "> $JAR_NAME 실행"
-
 nohup java -jar \
-    --spring.config.location=/home/ec2-user/app/application.yml \
-    $JAR_NAME &
+  $JAR_NAME \
+  --spring.config.location=$REPOSITORY/application.yml &
